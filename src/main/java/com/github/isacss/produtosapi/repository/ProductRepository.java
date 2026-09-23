483a0a8bdf6e8c8ffad5a7d3d7cb7d19caba565d
Isac-SS <isacsants679@gmail.com>
2026-09-23 11:32:33 -0300

    feat(product): add findByName

--- src/main/java/com/github/isacss/produtosapi/repository/ProductRepository.java
+++ src/main/java/com/github/isacss/produtosapi/repository/ProductRepository.java
@@ -3,6 +3,9 @@ package com.github.isacss.produtosapi.repository;
 import com.github.isacss.produtosapi.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
 
+    List<Product> findByName(String name);
 }
