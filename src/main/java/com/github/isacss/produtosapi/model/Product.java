3666018ca48fbc53de4b5a4ae387966200248e05
Isac-SS <isacsants679@gmail.com>
2026-09-23 10:59:18 -0300

    feat(product): create Product entity with JPA mapping

---
+++ src/main/java/com/github/isacss/produtosapi/model/Product.java
@@ -0,0 +1,65 @@
+package com.github.isacss.produtosapi.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @Column
+    private String id;
+    @Column
+    private String name;
+    @Column
+    private String description;
+    @Column
+    private BigDecimal price;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
